80f87b72253b50bac1aa305b25ea6d7bbc3a89d1
Kamil Pach <kamil_pach@whirlpool.com>
2024-04-25 08:36:49 +0200

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,10 @@
+def get_todos(filepath="todos.txt"):
+    """ Read a text file and return the losf of to-do items."""
+    with open(filepath, "r") as file:
+        todos_local = file.readlines()
+    return todos_local
+
+def write_todos(todos_arg, filepath="todos.txt" ):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
